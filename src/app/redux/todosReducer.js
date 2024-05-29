a9ecb8f1c64ddd270d50433db8a8d61c2b46c8bf
Shakib Imtiaz <107428455+shakib08@users.noreply.github.com>
2024-05-29 14:55:07 +0600

    Update todosReducer.js

---
+++ src/app/redux/todosReducer.js
@@ -0,0 +1,30 @@
+const initialState = {
+    tasks: []
+  };
+  
+  const todosReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'ADD_TASK':
+        return {
+          ...state,
+          tasks: [...state.tasks, action.payload]
+        };
+      case 'DELETE_TASK':
+        return {
+          ...state,
+          tasks: state.tasks.filter(task => task.id !== action.payload)
+        };
+      case 'EDIT_TASK':
+        return {
+          ...state,
+          tasks: state.tasks.map(task =>
+            task.id === action.payload.id ? { ...task, ...action.payload.taskData } : task
+          )
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default todosReducer;
+  
